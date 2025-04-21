41e93a0e96851ea7138196afe8ea060b11c78567
Luciana Rocha <lucianaregi@gmail.com>
2025-04-21 16:25:58 -0300

    feat(q4): implementa validação de CPF com exceção personalizada e tratamento com try/catch

---
+++ codigo/Questao4_TratamentoErros/TratamentoErros.Apresentacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using TratamentoErros.Core.Exceptions;
+using TratamentoErros.Core.Services;
+
+var validador = new ValidadorCpfService();
+
+var cpfsParaTestar = new List<string>
+{
+    "123.456.789-00", 
+    "11111111111",    
+    "39053344705",    
+    "529.982.247-25", 
+    "abcdefghijk",    
+    "1234567890",     
+    ""                
+};
+
+foreach (var cpf in cpfsParaTestar)
+{
+    try
+    {
+        Console.WriteLine($"Validando CPF: {cpf}");
+        validador.Validar(cpf);
+        Console.WriteLine("CPF válido\n");
+    }
+    catch (CpfInvalidoException ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}\n");
+    }
+}
---
+++ codigo/Questao4_TratamentoErros/TratamentoErros.Core/Services/ValidadorCpfService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TratamentoErros.Core.Exceptions;
+
+namespace TratamentoErros.Core.Services
+{
+    public class ValidadorCpfService
+    {
+        public void Validar(string cpf)
+        {
+            cpf = Regex.Replace(cpf ?? "", @"[^\d]", "");
+
+            if (cpf.Length != 11 || TodosDigitosIguais(cpf) || !DigitosVerificadoresValidos(cpf))
+            {
+                throw new CpfInvalidoException(cpf);
+            }
+        }
+
+        private bool TodosDigitosIguais(string cpf)
+        {
+            return cpf.All(d => d == cpf[0]);
+        }
+
+        private bool DigitosVerificadoresValidos(string cpf)
+        {
+            var numeros = cpf.Select(c => int.Parse(c.ToString())).ToArray();
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += numeros[i] * (10 - i);
+            int digito1 = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+            if (numeros[9] != digito1) return false;
+           
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += numeros[i] * (11 - i);
+            int digito2 = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+            return numeros[10] == digito2;
+        }
+    }
+}
